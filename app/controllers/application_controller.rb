4b3fe5410a1f2da3aa2b3169b8aefdb49c7b88c3
JustGritt <alexistan9712@gmail.com>
2022-06-28 21:21:20 +0200

    ⚡ Flash flag error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+    add_flash_types :danger, :success, :info, :warning
     helper_method :current_user, :logged_in?
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
