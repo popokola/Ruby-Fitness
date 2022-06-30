21e58fef1f88b7cca73c5f4423feec92fbf1dab2
Charles <charles258@hotmail.fr>
2022-06-30 22:40:16 +0200

    Route protection

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,7 @@
 module Admin
     class ApplicationController < Administrate::ApplicationController
         before_action :authenticate_admin
+        helper_method :current_user, :logged_in?
         def authenticate_admin
             # TODO Add authentication logic here.
         end
--- app/controllers/admin/forms_controller.rb
+++ app/controllers/admin/forms_controller.rb
@@ -1,11 +1,12 @@
 class FormsController < ApplicationController
     def index
     end
-    
-    def new
-        @user = User.new
+    if logged_in?
+        def new
+            @user = User.new
+        end
     end
-
+    
     def create
         @user = User.new(user_params)
         if @user.save
