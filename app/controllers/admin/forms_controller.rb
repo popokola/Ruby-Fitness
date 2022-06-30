158d7efa6a8b1aa4a5d1beaacde395da5737f43c
EC2 Default User <ec2-user@ip-172-31-46-120.eu-west-3.compute.internal>
2022-06-30 18:49:31 +0000

    add forms

---
+++ app/controllers/admin/forms_controller.rb
@@ -0,0 +1,18 @@
+class FormsController < ApplicationController
+    def index
+    end
+    
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "User created successfully"
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+end
