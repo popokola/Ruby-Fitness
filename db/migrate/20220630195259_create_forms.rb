6b0d36fbaa95f317fff03aa98830241a0db06ff6
Charles <charles258@hotmail.fr>
2022-06-30 22:18:46 +0200

    Rollback migration

--- db/migrate/20220630195259_create_forms.rb
+++ db/migrate/20220630195259_create_forms.rb
@@ -1,16 +1,4 @@
 class CreateForms < ActiveRecord::Migration[6.1]
-<<<<<<< HEAD
-    def change
-        create_table :forms do |t|
-        t.string :height
-        t.string :weight
-        t.string :activity
-
-        t.timestamps
-        end
-    end
-end
-=======
   def change
     create_table :forms do |t|
       t.string :height
@@ -19,4 +7,3 @@ end
     end
   end
 end
->>>>>>> 81c566d3aa72934f5363bf2813fae90f39c64822
